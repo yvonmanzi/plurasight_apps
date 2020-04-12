1b3de9d91a234ea4341604140b6d2809fbe78526
Yvon <ym2637@columbia.edu>
2020-04-12 17:21:01 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,45 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Color(0xff00796B),
-          accentColor: Color(0xff7C4DFF),
-          textTheme: TextTheme(
-              body1: TextStyle(
-            fontSize: 24,
-            fontStyle: FontStyle.italic,
-          ))),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Building Layouts with Flutter"),
-        ),
-        body: Center(
-          child: Text(
-            "Hello Flutter Layouts",
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.lightbulb_outline),
-          onPressed: () {
-            print("you rang");
-          },
-        ),
-        persistentFooterButtons: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add_comment),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.add_alarm),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.add_location),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+        title: "Container App",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Container")),
+            body: Container(
+              color: Colors.orange,
+            )));
   }
 }
